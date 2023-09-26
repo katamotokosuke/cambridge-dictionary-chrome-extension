d10fc322e3b4bb716810fe0119839e8339488175
katamotokosuke <katamotokosuke0605@gmail.com>
2023-09-26 18:57:32 -0400

    feature: add icon click behaver.
    if you click the icon where is in toolbar, you redirect to Cambridge Dictionary official site.

---
+++ src/service-worker.js
@@ -0,0 +1,7 @@
+
+chrome.action.onClicked.addListener(redirectOfficialSite);
+
+const ORIGIN_URL = "https://dictionary.cambridge.org"
+async function redirectOfficialSite() {
+    chrome.tabs.create({url: ORIGIN_URL});
+}
